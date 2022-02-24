d97bee4913872a37a8e1baa301b83ddec2eead2d
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2022-02-24 19:10:32 -0300

    aula_05

---
+++ Curso_JavaScript/aula_05.js
@@ -0,0 +1,45 @@
+// Variáveis - Tipos primitivos
+
+// Os principais são:
+// . number
+// . string
+// . boolean
+
+/* Outros tipos serão vistos ao longo do curso,
+ number - infinity e NaN
+ null
+ undefined
+ object  - Array 
+ function
+ */
+
+// Exemplos
+
+var n = 5
+console.log(typeof n);
+
+n = 'fruta'
+console.log(typeof n);
+
+console.log(typeof []);
+console.log(typeof {});
+
+var n1 = 6.5
+console.log(typeof n1);
+
+n1 = '6.5'
+console.log(typeof n1);
+
+var f = function(){};
+console.log(typeof f);
+
+var und = undefined
+console.log(typeof und)
+
+var na = NaN
+console.log(typeof na)
+
+var inf = Infinity
+console.log(typeof inf)
+
+console.log(typeof null)
